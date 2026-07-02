5083895d6b766d8a61468be0fda844ba289ef7d0
Ahmed Alharbi <48549687+ak-fr@users.noreply.github.com>
2026-07-03 00:37:20 +0200

    remove all the complications from the dictionary and keep it simple bounded linear probe

--- src/dict.c
+++ src/dict.c
@@ -23,15 +23,15 @@
 //  how many bits we store as a value in dictionary
 
 
-void print_m256i(__m256i a, char* text){
-  uint32_t A[8] = {0};
-  _mm256_storeu_si256((__m256i*)A, a);
-  printf("%s = ", text);
-  for (int i = 0; i<8; ++i) {
-    printf("%02x, ", A[i]);
-  }
-  puts("");
-}
+/* void print_m256i(__m256i a, char* text){ */
+/*   uint32_t A[8] = {0}; */
+/*   _mm256_storeu_si256((__m256i*)A, a); */
+/*   printf("%s = ", text); */
+/*   for (int i = 0; i<8; ++i) { */
+/*     printf("%02x, ", A[i]); */
+/*   } */
+/*   puts(""); */
+/* } */
 
 
 
@@ -45,7 +45,8 @@ void print_m256i(__m256i a, char* text){
 //-----------------------------------------------------//
 /* represent n in <= 6 char  */
 
-dict* dict_new(size_t nelements){
+dict *dict_new(size_t nelements) {
+  /// dict v5: simple bounded linear probing. avoid unnecessary complications.
   /// dict v4: we take part of the value as in index and store the rest
   ///          as a value. The value is maximally 32 bits 
   /// dict v3: we don't store values, we only store 64bit of the key
@@ -55,8 +56,8 @@ dict* dict_new(size_t nelements){
 
   dict* d = (dict *) aligned_alloc(ALIGNMENT, (sizeof(dict)));
 
-  int nslots_per_bucket = SIMD_LEN; /* See ../include/config.h */
-  size_t nslots = nelements;
+  /* int nslots_per_bucket = SIMD_LEN; /\* See ../include/config.h *\/ */
+  /* size_t nslots = nelements; */
 
   //+ todo does negative gives the correct result here?
   //nslots = nslots + (-nslots % nslots_per_bucket);
@@ -64,13 +65,11 @@ dict* dict_new(size_t nelements){
 
   /// Save configured variables as dictionary entries 
 
-  d->nbuckets = (nelements/nslots_per_bucket) + 1;
-  d->nslots_per_bucket = nslots_per_bucket;
-  d->nslots = (d->nbuckets)*(d->nslots_per_bucket);
+  d->nslots = nelements;
 
-  /* for huge pages : required memory for nslots is a mutlipe GPAGE_SIZE  */
-  d->nslots = ((d->nslots*sizeof(VAL_TYPE)) / GPAGE_SIZE ) * GPAGE_SIZE;
-  d->nslots = d->nslots / sizeof(VAL_TYPE);
+  /* /\* for huge pages : required memory for nslots is a mutlipe GPAGE_SIZE  *\/ */
+  /* d->nslots = ((d->nslots*sizeof(VAL_TYPE)) / GPAGE_SIZE ) * GPAGE_SIZE; */
+  /* d->nslots = d->nslots / sizeof(VAL_TYPE); */
     
   d->nelements = 0; /* how many elements currently in the dictionary */
   d->nelements_asked_to_be_inserted = 0;
@@ -94,22 +93,17 @@ dict* dict_new(size_t nelements){
   /* d->values = (VAL_TYPE*) malloc((nslots)*(sizeof(VAL_TYPE))); */
 
   // Ensure keys are zeroed
-  #pragma omp simd
-  for (size_t i = 0; i < nslots; ++i) 
+  //#pragma omp simd
+  for (size_t i = 0; i < (d->nslots); ++i) 
      d->values[i] = 0; // 0 if it is not occupied
 
   return d;
 }
 
-inline void dict_free(dict* d){
-  free(d->values);
-}
+inline void dict_free(dict *d) { free(d->values); }
+
 size_t dict_memory(size_t nelements){
-  /// return memory estimation of the dictionary size in BYTES
-  int nslots_per_bucket = SIMD_LEN; // we store 32 bits per value
-  size_t nslots = nelements;
-  nslots = nslots + (-nslots % nslots_per_bucket);
-  size_t estimate = nslots*(sizeof(VAL_TYPE)) + sizeof(dict);
+  size_t estimate = nelements*(sizeof(VAL_TYPE)) + sizeof(dict) + GPAGE_SIZE;
   
   return estimate;
 }
@@ -139,8 +133,7 @@ int dict_add_element_to(dict* d, u8* state){
   //                                                                          |
   // -------------------------------------------------------------------------+
   /* how many bytes do we need to index the buckets */
-  const int idx_size =   (int) ceil((log2(NSLOTS_MY_NODE) - log2(d->nslots_per_bucket))
-				   /8.0) ;
+  const int idx_size = (int)ceil(log2(NSLOTS_MY_NODE)/8.0) ;
 
   /* if (VAL_SIZE_BYTES + idx_size > N){ */
   /*   printf("ERROR at adding to dict! since VAL_SIZE_BYTES=%u, idx_size=%d, while N=%u\n", */
@@ -149,7 +142,7 @@ int dict_add_element_to(dict* d, u8* state){
   /* 	   N); */
   /* } */
   
-  ++(d->nelements_asked_to_be_inserted);
+  /* ++(d->nelements_asked_to_be_inserted); */
 
 
   /// Use linear probing to add element to the array d->values
@@ -159,7 +152,7 @@ int dict_add_element_to(dict* d, u8* state){
   /* printf("initially idx=0x%llx, idx_size=%d\n", idx, idx_size); */
 
   /* get the bucket number and scale the index */
-  idx = (idx % d->nbuckets) * d->nslots_per_bucket;
+  idx = idx % d->nslots;
 
   
   VAL_TYPE val = 0;
@@ -172,11 +165,7 @@ int dict_add_element_to(dict* d, u8* state){
   if (val == 0) return 0;
   
   // linear probing 
-  for (int i=0;
-       i<(NPROBES_MAX/SIMD_LEN)*SIMD_LEN; /* only Multiples of SIMD_LEN*/
-       ++i) {
-
-    
+  for (int i=0; i < NPROBES_MAX;  ++i) {
     // found an empty slot inside a bucket
     /* printf("idx=%llu, d->nslots=%lu\n", idx, d->nslots); */
     if (d->values[idx] == 0) { // found an empty slot
@@ -191,15 +180,11 @@ int dict_add_element_to(dict* d, u8* state){
       return 1;
     }
     
-
-
     ++idx;
     // reduce mod n->slots //
-    if (idx >= d->nslots ) /* we forgot the equal sign here */
+    if (idx >= d->nslots) /* we forgot the equal sign here */
       idx = 0;
   }
-  /* printf("missed entry at idx=%llu\n", idx_old); */
-  /* print_u16(&(d->values[idx_old]), NPROBES_MAX); */
   return 0; // element has been added
 }
 
@@ -225,13 +210,12 @@ int dict_has_elm(dict *d, u8 *state)
   //          (L bits) || discard || (VAL_SIZE bits)                          |
   // -------------------------------------------------------------------------+
   /* how many bytes do we need to index the buckets */
-  const int idx_size =  (int) ceil((log2(NSLOTS_MY_NODE) - log2(d->nslots_per_bucket))
-				   /8.0) ;
+  const int idx_size =  (int) ceil(log2(NSLOTS_MY_NODE) / 8.0) ;
 
   u64 idx = 0;
   memcpy(&idx, state, idx_size);
 
-  idx = (idx % d->nbuckets) * d->nslots_per_bucket;
+  idx = idx % d->nslots;
 
   VAL_TYPE val = 0;
   memcpy(&val,
@@ -240,58 +224,15 @@ int dict_has_elm(dict *d, u8 *state)
 
   /* 0 means empty, we have to ignore zero values  */
   if (val == 0) return 0;
-  
-
-  int is_key_found = 0;
-  // it's enough to check if the first element is empty
-  // in this version we don't need to check if we hit zero or not.
-  /* int empty_bucket = 0; //1 - _mm256_testz_si256(comp_vect_simd, comp_vect_simd); */
-  // we can remove one of the above variables 
-  //+ todo we need to adjust simd instruction according to the type 
-  REG_TYPE dict_keys_simd;// = _mm256_loadu_si256((__m256i*)  &(d->values[h]));
-  // val:u32 or val:16 depending on the N and L, as 32 or 16 will be stored
-  // and the other bits will be stored as index thus the dependency on L.
-  REG_TYPE lookup_key_simd = SIMD_SET1_VALTYPE(val); // (val, val, ..., val) 
-  //__m256i zero_vect = _mm256_setzero_si256(); // no need for this with buckets
-
 
   
   // loop at most NPROBES_MAX/SIMD_LEN since we load SIMD_LEN
   // elements from dictionary each loop.
-  for (size_t i=0; i< (int) (NPROBES_MAX/SIMD_LEN); ++i) {
-        
-    // we are relying that the val != 0, Pr(val !=0 ) = 1/(2^32)
-    // linear probing
-
-    // get new fresh keys from one bucket
-    dict_keys_simd = SIMD_LOAD_SI((REG_TYPE*)  &(d->values[idx]));
-
-
-    // -------------------------------------------------//
-    //                   TEST 1:                        //
-    /* is one the slots empty? then no point of probing */
-    //- ------------------------------------------------//
-    // no need for the commented instruction since we only check the first value
-    // comp_vect_simd = _mm256_cmpeq_epi64(dict_keys_simd, zero_vect);
-    // copy the value of the first slot of the bucket, check is it 0?
-    if (d->values[idx] == 0)
-      return 0;
-    
-    // -----------------------------------------------//
-    //                   TEST 2                       //
-    /*  Does key equal one of the slots?              */
-    //------------------------------------------------//
-
-    /* 0 no value found, otherwise a value was found  */
-    is_key_found = SIMD_CMP_VALTYPE(lookup_key_simd, dict_keys_simd);
-
-    if (is_key_found)
+  for (size_t i=0; i< (int) NPROBES_MAX; ++i) {
+    if (d->values[idx] == val)
       return 1; /* we will hash the whole message again */
 
-    // Linear probing
-    // update the index for keys load
-    idx += d->nslots_per_bucket; // move to the next bucket
-
+    ++idx; 
     if (idx >= d->nslots)
       idx = 0;
 
@@ -299,7 +240,6 @@ int dict_has_elm(dict *d, u8 *state)
     #ifdef NPROBES_COUNT
     ++(d->nprobes_lookup);
     #endif
-    //printf("inside next: step=%d, h=%lu, nslots=%lu\n", step, h, d->nslots);
   }
   return 0; // no element is found
 }
@@ -308,7 +248,7 @@ int dict_has_elm(dict *d, u8 *state)
 
 void dict_print(dict* d){
  
-  for (size_t b=0; b<(d->nslots); ++b) {
+  for (size_t b=0; b < (d->nslots); ++b) {
     printf("slot=%lu, "
 	   "key = 0x%016x\n",
 	   b,
